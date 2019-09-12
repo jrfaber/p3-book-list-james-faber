35e736e9550ec5db69024fa6090d1fe71289ca04
jrfaber <jrfaber@valdosta.edu>
2019-09-12 14:30:48 -0400

    added containers and App class to components folder

---
+++ src/containers/book-list.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+    renderList() {
+        // this.props.books works here thanks to the mapStateToProps function below
+        return this.props.books.map((book)=>{
+            return (
+                <li key={book.title} className="list-group-item">
+                    {book.title}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return(
+            <ul className="list-group col-sm-4">
+                {this.renderList()} 
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
