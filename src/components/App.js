a56a53ee1604172314da3d78141d26946bade902
jrfaber <jrfaber@valdosta.edu>
2019-09-12 15:00:38 -0400

    added onClick action to select a book--added details to the book clicked on with book-detail. Updated the css

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <BookList />
-      </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    <BookList />
+                </div>
+                <div className="col-md-4">
+                    <BookDetail />
+                </div>
+            </div>
+        </div>
     );
   }
 }   
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,31 +1,44 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
+
 
 class BookList extends Component {
     renderList() {
         // this.props.books works here thanks to the mapStateToProps function below
-        return this.props.books.map((book)=>{
+        return this.props.books.map(book => {
+            const cn =
+                this.props.activeBook &&
+                this.props.activeBook.title === book.title
+                    ? "list-group-item class-item-mine selected-item-mine"
+                    : "list-group-item class-item-mine";
+
             return (
-                <li key={book.title} className="list-group-item">
+                <li
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title}
+                    className={cn}
+                >
                     {book.title}
                 </li>
             );
         });
     }
-
     render() {
-        return(
-            <ul className="list-group col-sm-4">
-                {this.renderList()} 
-            </ul>
-        )
+        return <ul className="list-group">{this.renderList()}</ul>;
     }
 }
+// Anything returned from this function will end up as props on the 
+// BookList container
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+};
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
-        books: state.books
+        books: state.books,
+        activeBook: state.activeBook
     };
 }
-
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
